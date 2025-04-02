a0473a083be320df46444842c4bd3888ae473f9e
Syed Ali Quadri <syedaliquadri100@gmail.com>
2025-04-02 21:16:25 +0530

    Initialize Express server with middleware configuration and database connection setup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+const PORT = process.env.PORT || 8002;
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`The server is running on ${PORT}`)
+    });
+})
+.catch((err) => {
+    console.log("Error while running the server, ", err);
+})
